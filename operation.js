7e9ff4c6c5318ea3162d4aa5b2e950ff4b79316c
TSturtledove <roberttortorelli@yahoo.com>
2017-01-01 20:00:12 -0600

    added operations for setting the values of units based on dropdown options, and fixed the progression of page cards so it works again

--- operation.js
+++ operation.js
@@ -1,12 +1,20 @@
 //operation
 //advancement through steps
-var unit01 = new frame.basic();
-var unit02 = new frame.basic();
+var unit01;
+var unit02;
 
 $(".forward").click(function setName () {
+  var unitType01 = $("#unit01Type").val();
+  console.log(unitType01);
+  var unitType02 = $("#unit02Type").val();
+  console.log(unitType02);
+  unit01 = new frame[unitType01]();
+  unit02 = new frame[unitType02]();
   unit01.name = $("#pcName").val();
+  console.log(unit01);
   unit02.name = $("#aiName").val();
-})
+  console.log(unit02)
+});
 
 
 $("#player-setup").show();
@@ -26,12 +34,3 @@ $(".card__link").click(function(e) {
     $("." + nextCard).show();
   }
 });
-
-
-$(".forward").click( function(e){
-  var unitType = $(e.target).attr("value");
-  console.log(unitType)
-  var finalUnit = new frame.[unitType]();
-
-  console.log(finalUnit)
-})
